c5a0705f47f18fb6ca35affb80c04634b8e590b0
batalhadosherois <walen.almeida@gmail.com>
2017-05-08 23:51:57 -0300

    Update data-albe-timeline.js

--- data-albe-timeline.js
+++ data-albe-timeline.js
@@ -78,15 +78,17 @@ var data = [
 	}
 ];
 
-$(".img-responsive").click(function(){
+
+
+$(document).ready(function () {
+
+	$("#myTimeline").albeTimeline(data);
+	
+	$(".img-responsive").click(function(){
     var source=$(this).attr('src');
     var $dialog = $('<div></div>') 
          .html('<img src="'+source+'" width="200" height="200" alt="Milk(1 lit)">') 
          .dialog({ autoOpen: true, resizable: false, draggable: false, width: 600, height:600, modal: true, title: 'Create Your Own PopUp Window' }); 
 }); 
-
-$(document).ready(function () {
-
-	$("#myTimeline").albeTimeline(data);
 
 });
